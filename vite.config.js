5a69635a694e6f7e51bfbf82c45bc0957fc04eaa
GABRIEL  ALBUQUERQUE <gabriel.albuquerque@tecb2.com.br>
2026-03-01 03:23:06 -0300

    chore: add vite image optimizer plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      png: {
+        quality: 70,
+      },
+      jpeg: {
+        quality: 70,
+      },
+      jpg: {
+        quality: 70,
+      },
+      webp: {
+        quality: 70,
+      }
+    })
+  ],
 })
